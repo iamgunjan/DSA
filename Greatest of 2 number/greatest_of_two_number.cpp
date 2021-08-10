d72210c4c68169e42380695ff4523f16b218c20e
Gunjan <53567221+iamgunjan@users.noreply.github.com>
2021-08-10 20:14:25 +0530

    Create greatest_of_two_number.cpp

---
+++ Greatest of 2 number/greatest_of_two_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"1st Number: "; 
+    cin>>a;
+    cout<<"2nd Number: "; 
+    cin>>b;
+    if(a>b)
+    {
+	cout<<""<<a<<" is the largest";
+    }
+    else
+    {
+	cout<<""<<b<<" is the largest";
+    }
+    return 0;
+}
